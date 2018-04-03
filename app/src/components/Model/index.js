df4dafb923b50b1aa1d25040c47b6cf486bcb849
Sandeep <sandeep@applaunch.io>
2018-04-03 17:56:45 +0530

    model

---
+++ app/src/components/Model/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Model = (props) => {
+
+    return (
+        <div cloassName="model">
+            <div className="model-container">
+                { props.children }
+            </div>
+        </div>
+    );
+}
